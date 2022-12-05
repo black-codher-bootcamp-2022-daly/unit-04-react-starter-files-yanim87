10c04b6ebd3e0f27867a83d97e4c1ca7f1f09a29
Yetunde Animashawun <yanim87@gmail.com>
2022-12-05 19:28:15 +0000

    adding routes

--- routes-and-forms/src/index.js
+++ routes-and-forms/src/index.js
@@ -8,10 +8,14 @@ export default function App() {
       <ul className="breadcrumb">
         <li><Link to="/"> All Pets </Link></li>
         <li><Link to="/kittens"> Kittens </Link></li>
+        <li><Link to="/puppies"> Puppies </Link></li>
+        <li><Link to="/ducklings"> Ducklings </Link></li>
       </ul>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="kittens" element={<Kittens />} />
+        <Route path="puppies" element={<Puppies />} />
+        <Route path="ducklings" element={<Ducklings />} />
       </Routes>
   </Router>
   );
@@ -34,6 +38,19 @@ function Kittens() {
       <img src="imgs/kitten.jpg" alt="Kitten"/>
     </>
 }
-
+function Puppies () {
+  return <div>
+    <h2>Puppies</h2>
+    <section> This is the dogs page</section>
+    <img src="imgs/puppy.jpg" alt="Puppies"></img>
+  </div>
+}
+function Ducklings () {
+  return <div>
+    <h3>Ducklings</h3>
+    <section> This is a ducklings page</section>
+    <img src="imgs/duckling.jpg" alt="Ducklings"></img>
+  </div>
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App/>);
