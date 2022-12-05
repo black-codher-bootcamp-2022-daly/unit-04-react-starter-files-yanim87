7d710fc6649aca707c3bb81c11b20636f8849993
Yetunde Animashawun <yanim87@gmail.com>
2022-12-05 21:03:35 +0000

    building a form

--- routes-and-forms/src/components/PetForm.js
+++ routes-and-forms/src/components/PetForm.js
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const PetForm = () => {
-  const [name, setName] = useState('');
-  const [,setSubmitted] = useState(false);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [color, setColor]= useState("");
+  const [submitted, setSubmitted] = useState(false);
 
   function handleSubmit(event) {
     event.preventDefault();
     setSubmitted(true);
+    setAge(0);
+    setName("");
+    setColor("");
   }
 
   return (
@@ -15,16 +20,63 @@ const PetForm = () => {
       <h3>Please fill in your pet's details below</h3>
       <form onSubmit={(e) => handleSubmit(e)}>
         <div>
-          <label>Pet's Name:<input type="text" value={name} onChange={(e) => { setName(e.target.value); setSubmitted(false) }} /></label>
+          <label>
+            Pet's Name:
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+                setSubmitted(false);
+              }}
+            />
+          </label>
         </div>
-        <input type="submit" value="Submit" id="submit-button"/>
+        {/* add age label */}
+        <div>
+          <label>
+            Pet's Age:
+            <input
+              type="text"
+              value={age}
+              onChange={(e) => {
+                setAge(e.target.value);
+                setSubmitted(false);
+              }}
+            />
+          </label>
+        </div>
+        {/* set color label */}
+        <div style={{ backgroundColor: color, padding: "24px" }}>
+          Color: {color}
+        
+          <label>
+            Pet's Colour:
+            <input
+              type="text"
+              value={color}
+              onChange={(e) => {
+                setColor(e.target.value);
+                setSubmitted(false);
+              }}
+            />
+          </label>
+        </div>
+        <input type="submit" value="Submit" id="submit-button" />
       </form>
       <div id="results">
+
+       {
+          submitted ? <div> "Thank you for submitting" </div> : null
+        }
         <h3>Results</h3>
-            Name: {name}<br />
+        Name: {name}
+        Age: {age}
+        Colour: {color}
+        <br />
       </div>
     </div>
   );
-}
+};
 
 export default PetForm;
--- routes-and-forms/src/index.js
+++ routes-and-forms/src/index.js
@@ -1,6 +1,7 @@
 import {React} from "react";
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom'
+import PetForm from './components/PetForm.js';
 
 export default function App() {
   return (
@@ -25,6 +26,7 @@ function Home() {
   return <>
           <h1>All Pets</h1>
           <section>This is the page for all your pet needs</section>
+          <PetForm />
           <img src="imgs/kitten.jpg" alt="Kitten"/>
           <img src="imgs/puppy.jpg" alt="Puppy"/>
           <img src="imgs/duckling.jpg" alt="Duckling"/>
